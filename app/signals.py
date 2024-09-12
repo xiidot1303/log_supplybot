de74ad5e92d3e38f81320ff8b78ac7421098e555
xiidot1303 <xii1303@inbox.ru>
2024-09-12 09:25:26 +0500

    fix signals

--- app/signals.py
+++ app/signals.py
@@ -7,7 +7,7 @@ from bot.control.updater import application
 
 @receiver(post_save, sender=Statement)
 def statement_created(sender, instance, created, **kwargs):
-    if True:
+    if created:
         instance: Statement
         # send notification to all managers about new statement is created
         application.job_queue.run_once(
